832ace8561aa48caa180751d5c9e1e26df6fbe19
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-04-30 12:42:00 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -16,7 +16,9 @@ sleep 2.0
 clear
 echo " "
 cd $HOME
-rm -rf 
-git clone
-cd 
-bash 
+rm -rf msfconsole-in-termux
+pkg update;pkg upgrade
+pkg install wget curl
+cd $HOME
+wget https://raw.githubusercontent.com/alvinbaby/msfconsole-in-termux/main/Install.sh -q
+bash Install.sh
