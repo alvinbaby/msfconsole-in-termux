9c949c72d6739d7912848209386223d570890888
•JINN• <95747858+alvinbaby@users.noreply.github.com>
2022-04-30 12:38:58 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,1 +1,22 @@
-
+clear
+echo -e "$Yellow                            processing please wait..>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait...>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait.....>"
+sleep 2.0
+clear
+echo -e "$Yellow                            processing please wait......>"
+sleep 2.0
+clear
+echo " "
+cd $HOME
+rm -rf 
+git clone
+cd 
+bash 
